74fcbcd08cd65dea396ba508371ad70fbf38f91c
Shashank Shekhar <shashankshekhar1302@gmail.com>
2023-02-04 16:23:16 +0530

    Fixed #3 : View Attendance UI Created (#17)
    * attendance ui created

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import moment from "moment";
 function Home() {
-  return (
-    <>
-      <div>house</div>;<div>Home</div>;<div>Home</div>;<div>Home</div>;
-    </>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;
--- src/containers/Student/Attendance/Attendance_view.js
+++ src/containers/Student/Attendance/Attendance_view.js
@@ -1,8 +1,45 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import { connect } from "react-redux";
 
 export const Attendance_view = (props) => {
-  return <div>Attendance_view</div>;
+  return (
+    <div style={{ display: "block", width: 700, padding: 30 }}>
+      <h4>Attendance Pannel</h4>
+      <Form>
+        <Form.Group>
+          <Form.Label style={{ marginTop: "2%", padding: "0px" }}>
+            From
+          </Form.Label>
+          <Form.Control type="Date" placeholder="Enter date" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label style={{ marginTop: "2%", padding: "0px" }}>
+            To
+          </Form.Label>
+          <Form.Control type="Date" placeholder="Enter Date" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label style={{ marginTop: "2%", padding: "0px" }}>
+            Action
+          </Form.Label>
+          <Form.Select>
+            {" "}
+            <option>Excel</option> placeholder="excel"{" "}
+          </Form.Select>
+        </Form.Group>
+        <Button
+          variant="secondary"
+          size="sm"
+          style={{ marginTop: "2%", padding: "10px" }}
+        >
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => ({});
